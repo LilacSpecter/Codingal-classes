b30c1712230357a66aadfddca6b570b9d3c7233d
LilacSpecter <rowannenemesia@gmail.com>
2025-11-25 12:08:26 +0530

    update

--- Phython/try try.py
+++ Phython/try try.py
@@ -4,4 +4,16 @@
 #print("I am Iron man")
 #print("No, I am Tony Stark")
 #print("No, I am Poppy")
-#this is a comment
+#this is a comment 
+print("hello")
+print("I AM AN HAHAHAHAHA")
+Name = "Hanako"
+Age = "UNKNOWN"
+print(Name)
+print(Age)
+print("Method 1")
+print("My name is {} and my age is.....\n{}.".format(Name,Age))
+print("Method 2")
+print(f"My name {Name} and my age is.........\n{Age}.")
+print("Type of name is",(Name))
+print("Type of age is",(Age))
