ef979e4ded5d7622b9911efb20039b4d16e79b29
LilacSpecter <rowannenemesia@gmail.com>
2025-11-29 14:09:17 +0530

    update

--- Phython/trtl.py
+++ Phython/trtl.py
@@ -31,4 +31,33 @@ for i in range(4):
     board.right(90)
     i= i+1
 
+#Activity 2///
+
+import turtle
+turtle.Screen().bgcolor("white")
+board = turtle.Turtle()
+
+board.forward(100) # draw base
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+turtle.done()
 
