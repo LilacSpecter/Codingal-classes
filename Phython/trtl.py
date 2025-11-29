05ef124527a65da5e299229c8a758d0a4c9cd414
LilacSpecter <rowannenemesia@gmail.com>
2025-11-29 14:26:07 +0530

    update

--- Phython/trtl.py
+++ Phython/trtl.py
@@ -15,49 +15,70 @@
 
 #Activity 1\\\
 
-import turtle
+#import turtle
 
-turtle.Screen().bgcolor("teal")
+#turtle.Screen().bgcolor("teal")
 
-sc= turtle.Screen()
-sc.setup(400,400)
+#sc= turtle.Screen()
+#sc.setup(400,400)
 
-turtle.title("Welcome to Turtle Window")
+#turtle.title("Welcome to Turtle Window")
 
-board = turtle.Turtle()
+#board = turtle.Turtle()
 
-for i in range(4):
-    board.forward(100)
-    board.right(90)
-    i= i+1
+#for i in range(4):
+#    board.forward(100)
+#    board.right(90)
+#    i= i+1
 
 #Activity 2///
 
+#import turtle
+#turtle.Screen().bgcolor("white")
+#board = turtle.Turtle()
+
+#board.forward(100) # draw base
+
+#board.left(120)
+#board.forward(100)
+
+#board.left(120)
+#board.forward(100)
+
+#board.penup()
+#board.right(150)
+#board.forward(50)
+
+#board.pendown()
+#board.right(90)
+#board.forward(100)
+
+#board.right(120)
+#board.forward(100)
+
+#board.right(120)
+#board.forward(100)
+
+#turtle.done()
+
+#Activity 3///
+
 import turtle
-turtle.Screen().bgcolor("white")
-board = turtle.Turtle()
-
-board.forward(100) # draw base
-
-board.left(120)
-board.forward(100)
-
-board.left(120)
-board.forward(100)
-
-board.penup()
-board.right(150)
-board.forward(50)
-
-board.pendown()
-board.right(90)
-board.forward(100)
-
-board.right(120)
-board.forward(100)
-
-board.right(120)
-board.forward(100)
-
-turtle.done()
-
+t = turtle.Turtle()
+s = turtle.Screen()
+colors = ['red','purple','blue','green','orange','yellow']
+s.bgcolor('white')
+t.speed('fastest')
+t.hideturtle()
+while True:
+    for x in range(200):
+        t.pencolor(colors[x%len(colors)])
+        t.width(x/100 + 1)
+        t.forward(x)
+        t.left(59)
+    t.right(239)
+    for x in range(200, 0, -1):
+        t.pencolor('black')
+        t.width(x/100 + 7)
+        t.forward(x)
+        t.right(59)
