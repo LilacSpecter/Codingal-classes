e856523310e7201e98f499638a53544b27ab4ead
LilacSpecter <rowannenemesia@gmail.com>
2026-01-07 21:07:05 +0530

    update

--- Phython/FILE HANDLING/FILE.py
+++ Phython/FILE HANDLING/FILE.py
@@ -1,15 +1,49 @@
 #Activity 1
 #write to a file
-with open("sample.txt","w") as f :
-    f.write("Hello World\nThis is Phython file handling\nFile operations are imporant.")
+#with open("sample.txt","w") as f :
+#    f.write("Hello World\nThis is Phython file handling\nFile operations are imporant.")
 
     #read to a file
-with open("sample.txt", "r") as f:
-    content = f.read()
-    print("Full contend:\n", content)
+#with open("sample.txt", "r") as f:
+#    content = f.read()
+#    print("Full contend:\n", content)
 
 #Split content into words and store in a List
-words = content.split()
-print("\nSplit words:", words)
+#words = content.split()
+#print("\nSplit words:", words)
 
 # Close file (not needed when using 'with', it auto-closes.)
+
+# activity 1
+
+# open file and read it's contents
+file = open('Codingal.txt','r')
+print(file.read())
+file.close()
+
+#open file and read it's beginning 8 characters
+file = open('Codingal.txt','r')
+print("\n Read in parts \n")
+print(file.read(8))
+file.close()
+
+#apped your name and age in the file
+file = open('Codingal.txt','a')
+file.write("Hi! I am Penguin and i am 1 yr old.")
+file.close()
+
+#activity 2
+# read first line of file
+file = open('Codingal.txt','r')
+print("Reading multiple lines...")
+print(file.readline())
+print(file.readline())
+print(file.readline())
+file.close()
+
+#looping through alll lines of the file 
+file = open('Codingal.txt','r')
+print("Looping through the lines...")
+for lines in file:
+    print(line)
+file.close()
