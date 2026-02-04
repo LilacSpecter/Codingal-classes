5dd31e2d640f878e52a73f7f02305d4e5c332ed8
LilacSpecter <rowannenemesia@gmail.com>
2026-02-04 20:16:57 +0530

    update

--- Phython/main.py
+++ Phython/main.py
@@ -231,7 +231,6 @@
 #sety ={"banana", "orange"}
 #print("intersection", setx.intersection(sety))
 
-
 # 28/01/2026 #
 
 ## Module 14 lesson 1
@@ -250,39 +249,39 @@
 #  for i in range (1,n+1):
 #    sum+=i
 #  return sum
-
 #print(fun2(4))
 
 
 #3/2/2026
 #activity 1
-def printnumber(n):
-    iteration=0
-    print("the number entered by user is:", n)
-    iteration+=1
-    print("the number of iteration is :", iteration)
+#def printnumber(n):
+#    iteration=0
+#    print("the number entered by user is:", n)
+#    iteration+=1
+#    print("the number of iteration is :", iteration)
 
-printnumber(10)
+#printnumber(10)
 
 #activity 2
-def OnTime(n):
-    iteration=0
-    for i in range(1, n+1):
-        iteration+=1
-        print("the number of iteration is :", iteration)
+#def OnTime(n):
+#    iteration=0
+#    for i in range(1, n+1):
+#        iteration+=1
+#        print("the number of iteration is :", iteration)
 
-OnTime(4)
-OnTime(20)
+#OnTime(4)
+#OnTime(20)
 
-def ONSquareTime(n):
-    iteration=0
-    for i in range(0, n):
-      for j in range(0, n):
-          print("*",end="")
-          iteration+=1
-    print("the number of iteration:", iteration)
+#activity 3
+#def ONSquareTime(n):
+#    iteration=0
+#    for i in range(0, n):
+#      for j in range(0, n):
+#          print("*",end="")
+#          iteration+=1
+#    print("the number of iteration:", iteration)
 
-ONSquareTime(4)
+#ONSquareTime(4)
 
 
       
