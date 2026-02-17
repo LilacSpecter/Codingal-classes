96ca4daef73dda24de7ba7ba4854b329932edc73
LilacSpecter <rowannenemesia@gmail.com>
2026-02-17 21:16:19 +0530

    update

--- Phython/main.py
+++ Phython/main.py
@@ -392,3 +392,56 @@
 #    largest_num=numberStore
 #print("GCD is : ", largest_num)
 #print("HCF is : ", largest_num)
+
+#activity 1
+
+from math import sqrt
+
+number = int(input("Enter number : "))
+print("\n")
+
+if number > 1:
+
+    for i in range(2, int(sqrt(number))+1):
+
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+        else:
+            print(number, "is a prime number")
+
+else:
+    print(number, "is not a prime number")
+
+
+#activity 2
+def SieveOfEratosthenes(num):
+    prime = [True for i in range(num+1)]
+    p = 2
+    while (p * p <= num):
+        if (prime[p] == True):
+            for i in range(p * p, num+1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, num+1):
+        if prime[p]:
+            print(p, end="")
+num= int(input("Enter number"))
+SieveOfEratosthenes(num)
+print("above is prime number")
+
+#activity 3
+a = 3000
+for number in range (1,a+1):
+    c = 0
+    rev=0
+    temp = num
+    for i in range(1,temp+1):
+        if temp% i==0:
+            c+=1
+        if c==2:
+            while temp>0:
+                rev =rev*10+(temp%10)
+                temp//=10
+            if num==rev:
+                print(num,end="")
